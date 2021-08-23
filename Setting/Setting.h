81af76be35da34f1e26985e72fc8b59362bbf1e9
Tanakorn Phoochaliaw <tanakorn.ph@castcle.com>
2021-08-23 15:28:06 +0700

    Init project.

---
+++ Setting/Setting.h
@@ -0,0 +1,18 @@
+//
+//  Setting.h
+//  Setting
+//
+//  Created by Tanakorn Phoochaliaw on 23/8/2564 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Setting.
+FOUNDATION_EXPORT double SettingVersionNumber;
+
+//! Project version string for Setting.
+FOUNDATION_EXPORT const unsigned char SettingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Setting/PublicHeader.h>
+
+
